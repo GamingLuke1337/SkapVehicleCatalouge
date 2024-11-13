8330821781785938aa891e6b4ca91810dee57929
SkapMicke <161764761+SkapMicke@users.noreply.github.com>
2024-11-13 07:19:06 +0100

    Updated the script, Added lib that is a dependency
    dependency "lib" -- https://github.com/Jay60Dev/lib

--- client/main.lua
+++ client/main.lua
@@ -1,100 +1,73 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local previewVehicle = nil
+local previewVehicle;
+local vehiclesByCategory = Lib.Table.FilterByKey(QBCore.Shared.Vehicles, "category")
+local categoriesMenu = {}
 
-local function loadModel(model)
-    RequestModel(model)
-    while not HasModelLoaded(model) do
-        Wait(10)
-    end
-end
-
-local function spawnNPC()
-    loadModel(Config.NPC.model)
-    local npc = CreatePed(4, GetHashKey(Config.NPC.model), Config.NPC.coords.x, Config.NPC.coords.y, Config.NPC.coords.z, Config.NPC.coords.w, false, true)
-    FreezeEntityPosition(npc, true)
-    SetEntityInvincible(npc, true)
-    SetBlockingOfNonTemporaryEvents(npc, true)
-end
-
-local function showVehicleMenu(category)
-    local vehicleMenu = {}
-
-    for _, v in pairs(Config.Vehicles) do
-        if v.category == category then
-            table.insert(vehicleMenu, {
-                header = v.name .. " - $" .. v.price,
-                txt = "Brand: " .. (v.brand or "Unkown") .. " | Model: " .. v.model,
-                params = {
-                    event = "SkapVehicleCatalogue:client:previewVehicle",
-                    args = {model = v.model, price = v.price, name = v.name, category = v.category}
-                }
-            })
-        end
-    end
-
-    if #vehicleMenu > 0 then
-        exports['qb-menu']:openMenu(vehicleMenu)
-    else
-        QBCore.Functions.Notify("No vehicles were found in this cateogry.", "error")
-    end
-end
-
-
-RegisterNetEvent("SkapVehicleCatalogue:client:previewVehicle", function(data)
-    QBCore.Functions.Notify("Vehicle: " .. data.name .. " - Model: " .. data.model .. " - prize: " .. data.price .. " - Category: " .. data.category)
-
-    if previewVehicle then
-        DeleteEntity(previewVehicle)
-    end
-
-    local model = GetHashKey(data.model)
-    loadModel(model)
-    
-    previewVehicle = CreateVehicle(model, Config.PreviewCoords, 0.0, false, false)
-    SetEntityInvincible(previewVehicle, true)
-    SetEntityHeading(previewVehicle, 0.0)
-    FreezeEntityPosition(previewVehicle, true)
-
-    Citizen.CreateThread(function()
-        while DoesEntityExist(previewVehicle) do
-            local heading = GetEntityHeading(previewVehicle) + Config.PreviewRotationSpeed
-            SetEntityHeading(previewVehicle, heading)
-            Wait(0)
-        end
-    end)
-end)
-
-Citizen.CreateThread(function()
-    spawnNPC()
-    exports['qb-target']:AddTargetModel(Config.NPC.model, {
-        options = {
-            {
-                type = "client",
-                event = "SkapVehicleCatalogue:client:showCategoriesMenu",
-                icon = "fas fa-car",
-                label = "Show vehicle categories",
-            }
-        },
-        distance = 2.5,
+local openCategoryMenu = function(vehicles)
+    Lib.Menu({
+        id = "vehCatalogueVehicles",
+        title = "Vehicles",
+        options = vehicles,
     })
-end)
+end
 
-RegisterNetEvent("SkapVehicleCatalogue:client:showCategoriesMenu", function()
-    QBCore.Functions.Notify("When you choosed a vehicle, Remember the name of it so the cardealer can give you the right vehicle!")
-    local categoriesMenu = {}
-    for _, category in ipairs(Config.Categories) do
-        table.insert(categoriesMenu, {
-            header = category:sub(1,1):upper() .. category:sub(2),
-            params = {
-                event = "SkapVehicleCatalogue:client:showVehicleMenu",
-                args = {category = category}
-            }
-        })
+for k, v in pairs(vehiclesByCategory) do
+    categoriesMenu[#categoriesMenu+1] = {
+        title = k:gsub("^%l", string.upper),
+        onSelect = function()
+            openCategoryMenu(v)
+        end,
+    }
+    for _, vehicle in pairs(v) do
+        vehicle.title = ("%s %s - $%s"):format(vehicle.brand, vehicle.name, vehicle.price)
+        vehicle.onSelect = function()
+
+            if previewVehicle then Lib.DeleteEntity(previewVehicle) end
+
+            previewVehicle = Lib.SpawnVehicle({
+                model = vehicle.model,
+                coords = Config.PreviewCoords,
+                networked = true,
+                plate = "PREVIEW",
+            })
+
+            CreateThread(function()
+                while DoesEntityExist(previewVehicle) do
+                    local heading = GetEntityHeading(previewVehicle) + Config.PreviewRotationSpeed
+                    SetEntityHeading(previewVehicle, heading)
+                    Wait(0)
+                end
+            end)
+
+        end
     end
-    exports['qb-menu']:openMenu(categoriesMenu)
+    table.sort(v, function(a, b)
+        return (a.price or 9999999999) < (b.price or 9999999999)
+    end)
+end
+
+table.sort(vehiclesByCategory)
+table.sort(categoriesMenu, function(a, b)
+    return a.title < b.title
 end)
 
-RegisterNetEvent("SkapVehicleCatalogue:client:showVehicleMenu", function(data)
-    category = data.category
-    showVehicleMenu(category)
-end)
+Lib.SpawnPed({
+    model = Config.NPC.model,
+    coords = Config.NPC.coords,
+    freeze = true,
+    blockEvents = true,
+    invincible = true,
+    default = true,
+    targets = {
+        {
+            label = "Show Vehicle Catalogue",
+            onSelect = function()
+                Lib.Menu({
+                    id = "vehCatalogueCategories",
+                    title = "Vehicle Catalogue",
+                    options = categoriesMenu,
+                })
+            end
+        }
+    }
+})
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'Skap'
 description 'VehicleCatalouge for QBCore'
 version '1.0.0'
 
 shared_scripts {
+    '@lib/init.lua',
     'config.lua'
 }
 
@@ -13,6 +15,4 @@ client_scripts {
     'client/main.lua'
 }
 
-server_scripts {
-    'server/main.lua'
-}
+dependency "lib" -- https://github.com/Jay60Dev/lib
